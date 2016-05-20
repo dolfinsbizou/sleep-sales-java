b17aa58dfe07031e9a6c104da8db554cf55d2928
Emmanuel Leroux <lerouxe@etud.manastria.fr>
2016-05-20 18:05:45 +0200

    Main

--- projetJava24hBordeaux/src/main/java/main.java
+++ projetJava24hBordeaux/src/main/java/main.java
@@ -1,3 +1,9 @@
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +16,13 @@
  */
 public class main {
     public static void main(String args[]){
+        Socket socket;
+        try{
+            socket = new Socket(args[0], Integer.parseInt(args[1]));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        IOCommandes ioCommandes = new IOCommandes(socket);        
     }
     
 }
